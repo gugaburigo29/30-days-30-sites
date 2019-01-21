b91e22b0117a4d5bca71d5c583351ee1bd861ab8
guga <guga.burigo29@outlook.com>
2019-01-21 19:25:40 -0200

    Adiciona jsDoc no conversor de hex para rgba

--- portfolio/src/style/utils/toRGBA.js
+++ portfolio/src/style/utils/toRGBA.js
@@ -1,3 +1,9 @@
+/**
+ * @description Converte cor de hexadecial para rgba
+ * @example #FFFFFF -> rgba(255,255,255,1)
+ * @param {String} hex
+ * @param {Number} alpha
+ */
 export default (hex, alpha) => {
     const r = parseInt(hex.slice(1, 3), 16),
         g = parseInt(hex.slice(3, 5), 16),
